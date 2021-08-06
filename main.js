1b13227aff55a5860f61d8041400f38289fb41ee
Marius Heinrich <50840318+CodingMarius@users.noreply.github.com>
2021-08-06 17:31:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
 const express = require("express");
+
 const Gpio = require("onoff").Gpio;
+
 const cors = require("cors");
-const play = require("audio-sink");
-const load = require("audio-loader");
+
+var player = require('play-sound')(opts = {});
+
+// GLOBAL VARIABLES
 
 var belueftung = false;
+
 var beleuchtung = false;
+
 var audiosystem = false;
+
 var alarm = false;
 
 //LOAD EXPRESS
+
 const app = express();
 
 //EXPRESS PORT
+
 const port = 5000;
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/views`));
-app.listen(port, function() {
 
+app.listen(port, function () {
     console.log(`Application backend is listening on port: ${port}`);
-
 });
+
 
 app.get("/status", (req, res) => {
     class Values {
         constructor(belueftung, beleuchtung, audiosystem, alarm) {
+
             this.belueftung = belueftung;
+
             this.beleuchtung = beleuchtung;
+
             this.audiosystem = audiosystem;
+
             this.alarm = alarm;
         }
     }
@@ -37,12 +52,16 @@ app.get("/status", (req, res) => {
     return res.status(200).json(status);
 });
 
+
+
 app.post("/input/:id", (req, res) => {
     var id = req.params.id;
     switch (id) {
-
         case "1":
             belueftung = !belueftung;
+            let msg1 = {
+                msg: `Belüftung: ${belueftung}`
+            };
             let switch1 = new Gpio(23, 'out');
             if (belueftung) {
                 switch1.writeSync(0);
@@ -50,11 +69,14 @@ app.post("/input/:id", (req, res) => {
             if (!belueftung) {
                 switch1.writeSync(1);
             }
-            console.log({ msg: `Belüftung: ${belueftung}` });
-            return res.status(200).json({ msg: `Belüftung: ${belueftung}` });
+            console.log(msg1);
+            return res.status(200).json(msg1);
 
         case "2":
             beleuchtung = !beleuchtung;
+            let msg2 = {
+                msg: `Beleuchtung: ${beleuchtung}`
+            };
             let switch2 = new Gpio(24, 'out');
             if (beleuchtung) {
                 switch2.writeSync(0);
@@ -62,17 +84,45 @@ app.post("/input/:id", (req, res) => {
             if (!beleuchtung) {
                 switch2.writeSync(1);
             }
-            console.log({ msg: `Beleuchtung: ${beleuchtung}` });
-            return res.status(200).json({ msg: `Beleuchtung: ${beleuchtung}` });
+            console.log(msg2);
+            return res.status(200).json(msg2);
 
         case "3":
             audiosystem = !audiosystem;
-            res.status(200).json({ msg: `Audio-System: ${audiosystem}` });
-
+            let msg3 = {
+                msg: `Audio-System: ${audiosystem}`
+            };
+            console.log(msg3);
+            audiosys();
+            res.status(200).json(msg3);
             return;
+
         case "4":
             alarm = !alarm;
-            res.status(200).json({ msg: `Alarm: ${alarm}` });
+            let msg4 = {
+                msg: `Alarm: ${alarm}`
+            };
+            console.log(msg4);
+            alarmsys();
+            res.status(200).json(msg4);
             return;
     }
 });
+
+function audiosys() {
+    if (audiosystem) {
+        player.play('audio-system.wav', function (err) {
+            if (err) throw err
+        });
+    }
+    return null;
+}
+
+function alarmsys() {
+    if (alarm) {
+        player.play('audio.wav', function (err) {
+            if (err) throw err
+        });
+    }
+    return null;
+}
